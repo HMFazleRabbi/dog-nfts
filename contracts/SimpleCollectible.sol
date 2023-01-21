af9c9e84aec652e83620772e19dea119032df255
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-01-22 01:49:33 +0600

    Nft simple collectible

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -5,5 +5,17 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract SimpleCollectible is ERC721{
     
+    constructor() public  ERC721("Dogie", "DOG){
+        tokenCounter = 0;
+    }
+
+    function createCollectible(string memory tokenURI) public returns (uint256){
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter = tokenCounter + 1;
+        return newTokenId;
+    }
+
 }
 
